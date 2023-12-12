68640b06b1163a6a20dfbe555531544a64bc1b21
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-12-12 10:07:42 -0800

    Backend

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -24,6 +27,21 @@ app.put('/api/articles/:name/upvotes', (req, res) => {
     else{
         res.send('That article doesnt exist')
     }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments)
+    }
+    else{
+        res.send('That article doesnt exist')
+    }
+
 });
 
 app.listen(8000, () => {
