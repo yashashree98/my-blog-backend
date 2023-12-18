59d1d7e0574a763cdcf0a4491a8fea0b5b4352cc
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-12-18 11:33:54 -0800

    Fixed the backend issue

--- src/server.js
+++ src/server.js
@@ -7,56 +7,49 @@ app.use(express.json());
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
 
-    const article = await db.collection('articles').findOne( { name } );
+    const article = await db.collection('articles').findOne({ name });
 
-    if (article){
+    if (article) {
         res.json(article);
-    }
-    else{
+    } else {
         res.sendStatus(404);
     }
 });
 
-app.put('/api/articles/:name/upvotes', async (req, res) => {
+app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-    
+   
     await db.collection('articles').updateOne({ name }, {
         $inc: { upvotes: 1 },
     });
-
-    const article = await db.collection('articles').findOne( { name } );
-
-    if (article) {
-        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`)
-    }
-    else{
-        res.send('That article doesnt exist')
-    }
-});
-
-app.post('/api/articles/:name/comments', async(req, res) => {
-    const { name } = req.params;
-    const { postedBy, text } = req.body;
-
-    await db.collection('articles').updateOne( { name }, {
-        $push: { comments: {postedBy, text } },
-    });
-
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        article.comments.push({ postedBy, text });
-        res.send(article.comments)
+        res.json(article);
+    } else {
+        res.send('That article doesn\'t exist');
     }
-    else{
-        res.send('That article doesnt exist')
-    }
+});
 
+app.post('/api/articles/:name/comments', async (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } },
+    });
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
+        res.json(article);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
 });
 
 connectToDb(() => {
-    console.log('Successully connected to database!')
+    console.log('Successfully connected to database!');
     app.listen(8000, () => {
-        console.log('Server is listenig on port 8000');
+        console.log('Server is listening on port 8000');
     });
-});
+})
